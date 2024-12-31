bcd0d6b2f10396c764b4ed19c09e3344acf11d0f
hirrolot <hirrolot@gmail.com>
2024-12-31 15:15:15 +0500

    Move all the integer macros to `int_macros.h`

--- lib/int128_support.c
+++ lib/int128_support.c
@@ -3,14 +3,10 @@
 //
 // [1] <https://gcc.gnu.org/onlinedocs/gcc/_005f_005fint128.html>
 
+#include "int_macros.h"
+
 #include <ctype.h>
 #include <stddef.h>
-
-#define U128_MIN ((unsigned __int128)0)
-#define U128_MAX (~U128_MIN)
-
-#define I128_MIN (~I128_MAX)
-#define I128_MAX ((__int128)(U128_MAX >> 1))
 
 // The minimum size of a `char` array that is able to contain an arbitrary
 // integer in decimal notation. Currently, it is equal to the number of
@@ -59,9 +55,9 @@ static int impl_i128_print(
     }
 
     unsigned __int128 unsigned_n = 0;
-    if (I128_MIN == n) {
-        // `I128_MIN` is irrepresentable as a positive integer.
-        unsigned_n = (unsigned __int128)I128_MAX + 1;
+    if (C_INT_MIN(i128) == n) {
+        // `C_INT_MIN(i128)` is irrepresentable as a positive integer.
+        unsigned_n = (unsigned __int128)C_INT_MAX(i128) + 1;
     } else if (n < 0) {
         unsigned_n = (unsigned __int128)-n;
     } else {
@@ -84,7 +80,7 @@ static int impl_u128_scan_exn(
     unsigned __int128 result = 0;
     for (size_t i = 0; s[i] != '\0'; i++) {
         // Overflow.
-        FAIL_IF(result > U128_MAX / base);
+        FAIL_IF(result > C_INT_MAX(u128) / base);
         result *= base;
 
         const int digit = map_digit(s[i]);
@@ -93,7 +89,7 @@ static int impl_u128_scan_exn(
         // An out-of-range letter.
         FAIL_IF(digit >= base);
         // Overflow.
-        FAIL_IF(result > U128_MAX - (unsigned __int128)digit);
+        FAIL_IF(result > C_INT_MAX(u128) - (unsigned __int128)digit);
 
         result += (unsigned __int128)digit;
     }
@@ -117,13 +113,13 @@ static int impl_i128_scan_exn(
     FAIL_IF(-1 == impl_u128_scan_exn(s + offset, &result, base));
 
     // Overflow.
-    FAIL_IF(1 == sign && result > (unsigned __int128)I128_MAX);
+    FAIL_IF(1 == sign && result > (unsigned __int128)C_INT_MAX(i128));
     // Underflow.
-    FAIL_IF(-1 == sign && result > (unsigned __int128)I128_MAX + 1);
+    FAIL_IF(-1 == sign && result > (unsigned __int128)C_INT_MAX(i128) + 1);
 
-    if (-1 == sign && (unsigned __int128)I128_MAX + 1 == result) {
-        // `I128_MIN` is irrepresentable as a positive integer.
-        *x = I128_MIN;
+    if (-1 == sign && (unsigned __int128)C_INT_MAX(i128) + 1 == result) {
+        // `C_INT_MIN(i128)` is irrepresentable as a positive integer.
+        *x = C_INT_MIN(i128);
         return 0;
     }
 
--- lib/support.c
+++ lib/support.c
@@ -1,4 +1,5 @@
 #include "int128_support.c"
+#include "int_macros.h"
 
 #include <assert.h>
 #include <ctype.h>
@@ -13,73 +14,6 @@
 #include <caml/fail.h>
 #include <caml/memory.h>
 #include <caml/mlvalues.h>
-
-#define C_VALUE(namespace, x) C_VALUE_##namespace(x)
-#define C_VALUE_int(x)        Int_val(x)
-#define C_VALUE_u8(x)         ((uint8_t)Int_val(x))
-#define C_VALUE_u16(x)        ((uint16_t)Int_val(x))
-#define C_VALUE_u32(x)        ((uint32_t)Int32_val(x))
-#define C_VALUE_u64(x)        ((uint64_t)Int64_val(x))
-#define C_VALUE_u128(x)       u128_unwrap(x)
-#define C_VALUE_i8(x)         ((int8_t)Int_val(x))
-#define C_VALUE_i16(x)        ((int16_t)Int_val(x))
-#define C_VALUE_i32(x)        Int32_val(x)
-#define C_VALUE_i64(x)        Int64_val(x)
-#define C_VALUE_i128(x)       i128_unwrap(x)
-
-#define OCAML_VALUE(namespace, x) OCAML_VALUE_##namespace(x)
-#define OCAML_VALUE_int(x)        Val_int(x)
-#define OCAML_VALUE_u8(x)         Val_int((int)(x))
-#define OCAML_VALUE_u16(x)        Val_int((int)(x))
-#define OCAML_VALUE_u32(x)        caml_copy_int32((int32_t)(x))
-#define OCAML_VALUE_u64(x)        caml_copy_int64((int64_t)(x))
-#define OCAML_VALUE_u128(x)       u128_wrap(x)
-#define OCAML_VALUE_i8(x)         Val_int((int)(x))
-#define OCAML_VALUE_i16(x)        Val_int((int)(x))
-#define OCAML_VALUE_i32(x)        caml_copy_int32(x)
-#define OCAML_VALUE_i64(x)        caml_copy_int64(x)
-#define OCAML_VALUE_i128(x)       i128_wrap(x)
-
-#define C_INT_TY(namespace) C_INT_TY_##namespace
-#define C_INT_TY_int        int
-#define C_INT_TY_u8         uint8_t
-#define C_INT_TY_u16        uint16_t
-#define C_INT_TY_u32        uint32_t
-#define C_INT_TY_u64        uint64_t
-#define C_INT_TY_u128       unsigned __int128
-#define C_INT_TY_i8         int8_t
-#define C_INT_TY_i16        int16_t
-#define C_INT_TY_i32        int32_t
-#define C_INT_TY_i64        int64_t
-#define C_INT_TY_i128       __int128
-
-#define C_INT_MIN(namespace) C_INT_MIN_##namespace
-#define C_INT_MIN_int        INT_MIN
-#define C_INT_MIN_u8         UINT8_C(0)
-#define C_INT_MIN_u16        UINT16_C(0)
-#define C_INT_MIN_u32        UINT32_C(0)
-#define C_INT_MIN_u64        UINT64_C(0)
-#define C_INT_MIN_u128       U128_MIN
-#define C_INT_MIN_i8         INT8_MIN
-#define C_INT_MIN_i16        INT16_MIN
-#define C_INT_MIN_i32        INT32_MIN
-#define C_INT_MIN_i64        INT64_MIN
-#define C_INT_MIN_i128       I128_MIN
-
-#define C_INT_MAX(namespace) C_INT_MAX_##namespace
-#define C_INT_MAX_int        INT_MAX
-#define C_INT_MAX_u8         UINT8_MAX
-#define C_INT_MAX_u16        UINT16_MAX
-#define C_INT_MAX_u32        UINT32_MAX
-#define C_INT_MAX_u64        UINT64_MAX
-#define C_INT_MAX_u128       U128_MAX
-#define C_INT_MAX_i8         INT8_MAX
-#define C_INT_MAX_i16        INT16_MAX
-#define C_INT_MAX_i32        INT32_MAX
-#define C_INT_MAX_i64        INT64_MAX
-#define C_INT_MAX_i128       I128_MAX
-
-#define C_INT_BIT_WIDTH(namespace) (sizeof(C_INT_TY(namespace)) * CHAR_BIT)
 
 static bool is_valid_base(const int base) {
     switch (base) {
